56ce1fca43b97867be13ff3668fd9da2f675fe7e
DevinLevy <analescheok@hotmail.com>
2013-09-15 23:00:07 -0600

    new project

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeekThreeProject;
+using System.Linq;
+using System.Data;
+using System.Data.Objects;
+using System.Data.Entity;
+using System.Data.Services.Client;
+//using System.Data.Entity.Validation;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int value = 1;
+            using (var context = new Model1Container())
+            {
+                Lab lab = new Lab();
+                
+                   
+            }
+
+
+            
+        }
+    }
+}
---
+++ WeekThreeProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeekThreeProject.Global" Language="C#" %>
